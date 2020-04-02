6c83fde875cab68e5a62e31eb6218ad7cf84b823
Brock Ryan <brock.ryan2@gmail.com>
2020-04-02 00:30:18 -0400

    Update sport.py

--- sport.py
+++ sport.py
@@ -3,6 +3,8 @@ class Sport():
 
     def __init__(self, sport):
         self.sport = sport
+        self._base_stats = Stats(sport)
+        self._ruleset    = Rules(sport)
 
         if self.sport = "football":
 
