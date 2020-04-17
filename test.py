16f8c990dc2b699df11bc43759935b4d52b08473
Brock Ryan <brock.ryan2@gmail.com>
2020-04-17 02:29:59 -0400

    refactor with zip

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from itertools import zip_longest
 
 def is_iterable(var):
     return True if hasattr(var, '__iter__') else False
@@ -16,74 +17,86 @@ def is_empty(var):
 
 class Name():
 
-    def __init__(self, *args, pair_flag=True):
-        num_args = len(args)
+    def __init__(self, *args, attributes=None, values=None, pair_flag=True):
+        print(values[0])
 
-        if num_items_is_even(num_args) and pair_flag:
+        if attributes is not None and is_iterable(attributes):
+            zipped = zip_longest(attributes, values, fillvalue=None)
+            for attribute, value in zipped:
+                print("attribute = ", attribute, " |  value = ", value)
+                self.__dict__[attribute] = value
 
-            for i in range(0, num_args, 2):
-                if is_hashable(args[i]):
-                    self.__dict__[args[i]] = args[i + 1]
+        else:
+            num_args = len(args)
 
-                else:
-                    if is_iterable(args[i]):
-                        for n in range(0, len(args[i])):
-                            if is_hashable(args[i][n]) and is_hashable(args[i][n+1]):
-                                self.__dict__[args[i][n]] = args[i][n + 1]
+            if num_items_is_even(num_args) and pair_flag:
 
-                            else:
-                                self.__dict__[None] = args[i][n]
+                for i in range(0, num_args, 2):
+                    if is_hashable(args[i]):
+                        self.__dict__[args[i]] = args[i + 1]
 
+                    else:
+                        if is_iterable(args[i]):
+                            for n in range(0, len(args[i])):
+                                if is_hashable(args[i][n]) and is_hashable(args[i][n+1]):
+                                    self.__dict__[args[i][n]] = args[i][n + 1]
 
-
+                                else:
+                                    self.__dict__[None] = args[i][n]
 
             #else:
                 #self.__dict__[item] = None
 
+            else:
+                for arg in args:
+                    if is_iterable(arg) and is_empty(arg):
+                        continue
 
-
-        else:
-            for arg in args:
-                if is_iterable(arg) and is_empty(arg):
-                    continue
-
-                elif type(arg) is dict:
+                    elif type(arg) is dict:
                         self.__dict__ = arg
 
-                elif type(arg) is str:
+                    elif type(arg) is str:
                         self.__dict__[arg] = None
 
-                elif len(arg) % 2 == 0:
-                    for i in range(0, len(arg), 2):
-                        self.__dict__[arg[i]] = arg[i + 1]
+                    elif len(arg) % 2 == 0:
+                        for i in range(0, len(arg), 2):
+                            self.__dict__[arg[i]] = arg[i + 1]
 
-                else:
-                    for item in arg:
-                        if hasattr(item, '__iter__') and len(item) % 2 == 0:
-                            for i in range(0, len(item), 2):
-                                self.__dict__[item[i]] = item[i + 1]
+                    else:
+                        for item in arg:
+                            if hasattr(item, '__iter__') and len(item) % 2 == 0:
+                                for i in range(0, len(item), 2):
+                                    self.__dict__[item[i]] = item[i + 1]
 
-                        elif is_iterable(item) and not is_hashable(item):
-                            for i in item:
-                                self.__dict__[i] = None
+                            elif is_iterable(item) and not is_hashable(item):
+                                for i in item:
+                                    self.__dict__[i] = None
 
-                        else:
-                            self.__dict__[None] = None
+                            else:
+                                self.__dict__[None] = None
                 #else:
                     #self.__dict__[arg] = None
 
+#a = Name("Bob")
+#print(a.__dict__)
+#
+#b = Name("Bob", "cat")
+#print(b.__dict__)
+#
+#c = Name(['type', 'null', 'category', 'I don\'t know'])
+#print(c.__dict__)
+#
+#d = Name({'one':1, 'two': 2})
+#print(d.__dict__)
+#
+##e = Name([1,2,3],'apple', "monkey", ('see', 'do'))
+##print(e.__dict__)
+#
+#f = Name(attributes=['ten', 'nine', 'eight'], values=['10', '9'])
+#print(f.__dict__)
 
-a = Name("Bob")
-print(a.__dict__)
+tuple1 = ("this is an attribute", "Truthy", 1, 0.01, False, b'\xf0\xf1\xf2')
+list_1 = ["yay", "fAlSe", 0, 0.99, 'TrUe']
 
-b = Name("Bob", "cat")
-print(b.__dict__)
-
-c = Name(['type', 'null', 'category', 'I don\'t know'])
-print(c.__dict__)
-
-d = Name({'one':1, 'two': 2})
-print(d.__dict__)
-
-e = Name([1,2,3],'apple', "monkey", ('see', 'do'))
-print(e.__dict__)
+complex_example = Name(attributes=(tuple1), values=list_1)
+print(complex_example.__dict__)
